ec588916562135aad0b19f2b31670ecb19f1db22
George Hawkins <george-hawkins@users.noreply.github.com>
2018-11-25 20:08:01 +0100

    Updated siteConfig to match Github Pages setup.

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -9,17 +9,17 @@
 // site configuration options.
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  title: 'ARF Drone', // Title for your website.
+  tagline: 'Unused',
+  url: 'https://george-hawkins.github.io', // Your website URL
+  baseUrl: '/arf-drone/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'unused',
+  organizationName: 'george-hawkins',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
@@ -39,7 +39,7 @@ const siteConfig = {
   },
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+  copyright: `Copyright © ${new Date().getFullYear()} George Hawkins`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
